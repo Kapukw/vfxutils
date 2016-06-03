41f5d987c961124f8f7809da474f2cb476d9f3d7
Igor Frolov <kapukw@gmail.com>
2016-06-03 19:42:48 +0500

    Refactoring

--- utils/GridMaker.py
+++ utils/GridMaker.py
@@ -1,39 +1,60 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageChops
 
-out_im = Image.new('RGBA', (2048, 2048))
-out_nm = Image.new('RGBA', (2048, 2048))
-out_bg_nm = Image.new('RGB', (2048, 2048))
-out_alpha_mask = Image.new('RGB', (2048, 2048))
+frame_size = (256, 256)
+grid_size = (8, 8)
+
+out_grid_size = (grid_size[0] * frame_size[0], grid_size[1] * frame_size[1])
+
+out_grid = Image.new('RGBA', out_grid_size)
+out_nm_grid = Image.new('RGBA', out_grid_size)
+background_nm_grid = Image.new('RGB', out_grid_size)
+alpha_mult_grid = Image.new('L', out_grid_size)
 
 x = 0
 y = 0
-for frameIdx in xrange(0, 64):
-    frame_im = Image.open("C:/Projects/pfx/images/test2.{}.png".format(str(10+frameIdx).zfill(4)))
+for frameIdx in xrange(0, grid_size[0] * grid_size[1]):
+
+    frame = Image.open("C:/Projects/pfx/images/test2.{}.png".format(str(10+frameIdx).zfill(4)))
+    frame.thumbnail(frame_size)
+
     frame_nm = Image.open("C:/Projects/pfx/images/test2.PhoenixFD_normals.{}.png".format(str(10+frameIdx).zfill(4)))
-    background_nm = Image.open("C:/Projects/pfx/images/background_nm.png")
-    a_mask = Image.open("C:/Projects/pfx/images/alpha_mask.png")
+    frame_nm.thumbnail(frame_size)
 
-    frame_im.thumbnail((256, 256))
-    frame_nm.thumbnail((256, 256))
-    background_nm.thumbnail((256, 256))
-    a_mask.thumbnail((256, 256))
+    background_nm = Image.open("C:/Projects/vfxutils/textures/background_nm.tga")
+    background_nm.thumbnail(frame_size)
 
-    out_im.paste(frame_im, (x * 256, y * 256))
-    out_nm.paste(frame_nm, (x * 256, y * 256))
-    out_bg_nm.paste(background_nm, (x * 256, y * 256))
-    out_alpha_mask.paste(a_mask, (x * 256, y * 256))
+    alpha_mult = Image.open("C:/Projects/vfxutils/textures/alpha_mask.tga")
+    alpha_mult.thumbnail(frame_size)
+
+    location = (x * frame_size[0], y * frame_size[1])
+
+    out_grid.paste(frame, location)
+    out_nm_grid.paste(frame_nm, location)
+    background_nm_grid.paste(background_nm, location)
+    alpha_mult_grid.paste(alpha_mult, location)
 
     x += 1
-    if x >=8:
+    if x >= grid_size[0]:
         x = 0
         y += 1
 
-r, g, b, a = out_nm.split()
+# Invert normal.x 
+r, g, b, a = out_nm_grid.split()
 r = ImageOps.invert(r)
-out_nm = Image.merge("RGBA", (r, g, b, a))
 
-out_im.save("y:/art/source/particles/textures/special/anim_test2.tif")
-out_nm.save("y:/art/source/particles/textures/special/anim_test2_nm.tif")
-out_bg_nm.save("y:/art/source/particles/textures/special/anim_test2_bg_nm.tif")
-out_alpha_mask.save("y:/art/source/particles/textures/special/anim_test2_a_msk.tif")
-a.save("y:/art/source/particles/textures/special/anim_test2_a.tif")
+# Make smooth borders
+a = ImageChops.multiply(a, alpha_mult_grid)
+
+# Add smooth backgound to invisible parts for good encoding
+nm_rgb_grid = ImageChops.composite(Image.merge("RGB", (r, g, b)), background_nm_grid, a)
+r, g, b = nm_rgb_grid.split()
+out_nm_grid = Image.merge("RGBA", (r, g, b, a))
+
+out_nm_grid.save("y:/art/source/particles/textures/special/anim_test2_nm.tga")
+
+# Make smooth borders
+r, g, b, a = out_grid.split()
+a = ImageChops.multiply(a, alpha_mult_grid)
+out_grid = Image.merge("RGBA", (r, g, b, a))
+
+out_grid.save("y:/art/source/particles/textures/special/anim_test2.tga")
